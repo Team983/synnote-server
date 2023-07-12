19d5f8a227aeafaa09d0292136565c712d11c237
ecvheo1 <ecvheo@naver.com>
2023-07-12 16:33:36 +0900

    init: kotlin spring boot project setup

---
+++ src/main/kotlin/com/synnote/record/RecordApplication.kt
@@ -0,0 +1,11 @@
+package com.synnote.record
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecordApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecordApplication>(*args)
+}
---
+++ src/test/kotlin/com/synnote/record/RecordApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.synnote.record
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecordApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
