50825d4f7c7e3224d4585920a76e4a7079ebdf29
ecvheo1 <ecvheo@naver.com>
2023-07-28 16:57:54 +0900

    :art: code type 변경

--- src/main/kotlin/com/team983/synnote/common/dto/BaseResponse.kt
+++ src/main/kotlin/com/team983/synnote/common/dto/BaseResponse.kt
@@ -3,7 +3,7 @@ package com.team983.synnote.common.dto
 import com.team983.synnote.common.status.ResultCode
 
 data class BaseResponse<T>(
-    val resultCode: Int = ResultCode.SUCCESS.code,
+    val resultCode: String = ResultCode.SUCCESS.code,
     val message: String = ResultCode.SUCCESS.msg,
     val data: T? = null
 )
--- src/main/kotlin/com/team983/synnote/common/status/ResultCode.kt
+++ src/main/kotlin/com/team983/synnote/common/status/ResultCode.kt
@@ -1,8 +1,11 @@
 package com.team983.synnote.common.status
 
-enum class ResultCode(val code: Int, val msg: String) {
-    SUCCESS(0, "OK"),
-    ERROR(99, "ERROR"),
+enum class ResultCode(val code: String, val msg: String) {
+    // Common
+    SUCCESS("C_00", "OK"),
+    ERROR("C_99", "ERROR"),
+    INVALID_INPUT_VALUE("C_01", "적절하지 않은 요청 값입니다."),
 
-    INVALID_INPUT_VALUE(1, "적절하지 않은 요청 값입니다.")
+    // User
+    USER_NOT_FOUND("U_01", "해당 유저 엔티티를 찾을 수 없습니다.")
 }
