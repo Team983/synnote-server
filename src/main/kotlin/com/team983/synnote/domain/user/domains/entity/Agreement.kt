83b717622b7b4b10c8db7c4d6dc24f5e3e719bc4
ecvheo1 <ecvheo@naver.com>
2023-07-28 16:18:38 +0900

    :fire: LastModifiedDate 애노테이션 제거

--- src/main/kotlin/com/team983/synnote/domain/user/domains/entity/Agreement.kt
+++ src/main/kotlin/com/team983/synnote/domain/user/domains/entity/Agreement.kt
@@ -2,18 +2,14 @@ package com.team983.synnote.domain.user.domains.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
-import org.springframework.data.annotation.LastModifiedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity
-@EntityListeners(AuditingEntityListener::class)
 @Table(name = "agreement")
 class Agreement(
     @Id
@@ -32,15 +28,12 @@ class Agreement(
     @Column(name = "service_improvement", nullable = false, columnDefinition = "TINYINT")
     var serviceImprovement: Boolean = false,
 
-    @LastModifiedDate
     @Column(name = "privacy_policy_date", nullable = false)
-    var privacyPolicyDate: LocalDateTime? = null,
+    var privacyPolicyDate: LocalDateTime? = LocalDateTime.now(),
 
-    @LastModifiedDate
     @Column(name = "terms_and_cons_date", nullable = false)
-    var termsAndConsDate: LocalDateTime? = null,
+    var termsAndConsDate: LocalDateTime? = LocalDateTime.now(),
 
-    @LastModifiedDate
     @Column(name = "service_improvement_date", nullable = false)
-    var serviceImprovementDate: LocalDateTime? = null
+    var serviceImprovementDate: LocalDateTime? = LocalDateTime.now()
 )
