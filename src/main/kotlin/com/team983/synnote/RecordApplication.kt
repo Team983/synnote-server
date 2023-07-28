5f9b2caf1236729caa7a0bd0b1c96625be1466b1
ecvheo1 <ecvheo@naver.com>
2023-07-28 13:41:37 +0900

    :sparkles: EnableJpaAuditing 애노테이션 추가

--- src/main/kotlin/com/team983/synnote/RecordApplication.kt
+++ src/main/kotlin/com/team983/synnote/RecordApplication.kt
@@ -4,9 +4,11 @@ import com.team983.synnote.common.config.AmazonS3Config
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableConfigurationProperties(AmazonS3Config::class)
+@EnableJpaAuditing
 class SynnoteApplication
 
 fun main(args: Array<String>) {
