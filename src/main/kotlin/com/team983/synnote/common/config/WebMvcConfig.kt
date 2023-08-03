789e26dc3ad3a4995f32074ffa666130597f5418
ecvheo1 <ecvheo@naver.com>
2023-08-03 16:57:05 +0900

    :sparkles: CORS 설정

--- src/main/kotlin/com/team983/synnote/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/team983/synnote/common/config/WebMvcConfig.kt
@@ -2,11 +2,14 @@ package com.team983.synnote.common.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class WebMvcConfig : WebMvcConfigurer {
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/")
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui/")
@@ -14,8 +17,15 @@ class WebMvcConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("*")
+            .allowedOrigins(
+                "https://www.synnote.com",
+                "http://localhost:3000",
+                "https://dev.synnote.com",
+                "https://synnote-production.auth.ap-northeast-2.amazoncognito.com",
+                "https://synnote.auth.ap-northeast-2.amazoncognito.com"
+            )
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
