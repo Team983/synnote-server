5eb7f5cc37f5dfc726a5a67d797e0776c213de1c
ecvheo1 <ecvheo@naver.com>
2023-08-04 20:32:57 +0900

    :art: / 삭제

--- src/main/kotlin/com/team983/synnote/domain/recording/interfaces/controller/RecordingController.kt
+++ src/main/kotlin/com/team983/synnote/domain/recording/interfaces/controller/RecordingController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/v1/recording")
+@RequestMapping("api/v1/recording")
 @RestController
 class RecordingController(
     private val recordingFacade: RecordingFacade
