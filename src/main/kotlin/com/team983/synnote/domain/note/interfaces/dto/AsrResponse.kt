f9d668dad64fdbefbefadb6dc92af4c36ba7cfc2
ecvheo1 <ecvheo@naver.com>
2023-10-28 13:31:30 +0900

    :recycle: 전사 요청 응답 dto 구조 리팩토링

--- src/main/kotlin/com/team983/synnote/domain/note/interfaces/dto/AsrResponse.kt
+++ src/main/kotlin/com/team983/synnote/domain/note/interfaces/dto/AsrResponse.kt
@@ -3,6 +3,7 @@ package com.team983.synnote.domain.note.interfaces.dto
 import com.team983.synnote.domain.note.domains.entity.Recording
 import com.team983.synnote.domain.note.domains.enums.Status
 
+// ASR 요청에 대한 응답
 data class AsrRequestResponse(
     val noteId: Long,
     val filename: String,
@@ -18,30 +19,33 @@ data class AsrRequestResponse(
     }
 }
 
+// Whisper ASR 결과에 대한 응답
 data class AsrResultResponse(
     val noteId: Long,
     val texts: List<String>
 )
 
+// Whisperx ASR 결과에 대한 응답
 data class WhisperxAsrResultResponse(
     val noteId: Long,
     val segments: List<Segment>,
     val language: String
-)
-
-data class Segment(
-    val text: String,
-    val start: Long,
-    val end: Long,
-    val speaker: String
 ) {
-    val startInMilliseconds: Long
-        get() = start * 1000
+    data class Segment(
+        val text: String,
+        val start: Long,
+        val end: Long,
+        val speaker: String
+    ) {
+        val startInMilliseconds: Long
+            get() = start * 1000
 
-    val endInMilliseconds: Long
-        get() = end * 1000
+        val endInMilliseconds: Long
+            get() = end * 1000
+    }
 }
 
+// ASR 에러에 대한 응답
 data class AsrErrorResponse(
     val noteId: Long,
     val message: String,
