fa8af436c266fff04a3fa07c91aa1ccd26f9f953
ecvheo1 <ecvheo@naver.com>
2023-10-31 12:21:26 +0900

    :wrench: redis 설정값 설정

---
+++ src/main/kotlin/com/team983/synnote/common/config/RedisConfig.kt
@@ -0,0 +1,66 @@
+package com.team983.synnote.common.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.team983.synnote.domain.notification.dtos.SummaryCompletedDto
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisOperations
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.listener.RedisMessageListenerContainer
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Value("\${spring.data.redis.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    private var port: Int = 0
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory(host, port)
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().registerModule(
+            KotlinModule.Builder()
+                .withReflectionCacheSize(512)
+                .configure(KotlinFeature.NullToEmptyCollection, false)
+                .configure(KotlinFeature.NullToEmptyMap, false)
+                .configure(KotlinFeature.NullIsSameAsDefault, false)
+                .configure(KotlinFeature.StrictNullChecks, false)
+                .build()
+        )
+    }
+
+    @Bean
+    fun eventRedisOperations(
+        redisConnectionFactory: RedisConnectionFactory,
+        objectMapper: ObjectMapper
+    ): RedisOperations<String, SummaryCompletedDto> {
+        val jsonRedisSerializer = Jackson2JsonRedisSerializer(objectMapper, SummaryCompletedDto::class.java)
+        val eventRedisTemplate = RedisTemplate<String, SummaryCompletedDto>()
+        eventRedisTemplate.setConnectionFactory(redisConnectionFactory)
+        eventRedisTemplate.setKeySerializer(RedisSerializer.string())
+        eventRedisTemplate.setValueSerializer(jsonRedisSerializer)
+        eventRedisTemplate.setHashKeySerializer(RedisSerializer.string())
+        eventRedisTemplate.setHashValueSerializer(jsonRedisSerializer)
+        return eventRedisTemplate
+    }
+
+    @Bean
+    fun redisMessageListenerContainer(redisConnectionFactory: RedisConnectionFactory): RedisMessageListenerContainer {
+        val redisMessageListenerContainer = RedisMessageListenerContainer()
+        redisMessageListenerContainer.setConnectionFactory(redisConnectionFactory)
+        return redisMessageListenerContainer
+    }
+}
