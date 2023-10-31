6b77320eeac4472472c418052290308aaf49fcda
ecvheo1 <ecvheo@naver.com>
2023-10-31 15:24:13 +0900

    :fire: 불필요한 코드 제거

--- src/main/kotlin/com/team983/synnote/domain/notification/interfaces/NotificationController.kt
+++ src/main/kotlin/com/team983/synnote/domain/notification/interfaces/NotificationController.kt
@@ -6,7 +6,6 @@ import org.springframework.data.redis.core.RedisOperations
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
@@ -18,11 +17,11 @@ class NotificationController(
     private val notificationService: NotificationService
 ) {
 
-    @PostMapping("/send-data/{id}")
-    fun sendData(@PathVariable id: Long) {
-        val iid: String = id.toString()
-        this.eventRedisOperations.convertAndSend(getChannelName(iid), SummaryCompletedDto(iid, 1))
-    }
+//    @PostMapping("/send-data/{id}")
+//    fun sendData(@PathVariable id: Long) {
+//        val iid: String = id.toString()
+//        this.eventRedisOperations.convertAndSend(getChannelName(iid), SummaryCompletedDto(iid, 1))
+//    }
 
     @GetMapping(value = ["/subscribe/{userId}"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
     fun subscribe(@PathVariable userId: String): SseEmitter? {
